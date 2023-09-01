89f19bc9f9bd681b6383c795dd0590fff1c70ae0
vadirajabhat1978 <50026814+vadirajabhat1978@users.noreply.github.com>
2023-09-01 17:25:34 +0530

    Update Expenses.js

--- Expenses.js
+++ Expenses.js
@@ -8,18 +8,17 @@ import { useState } from 'react';
 function Expenses(props) {
     //useState
     const [filteredYear, setFilteredYear] = useState('2020');
+    const filteredExpenses = props.expenses.filter(expense => expense.date.getFullYear().toString() === filteredYear);
 
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
-
-        props.onYearSelected(selectedYear);
     };
 
     return (
         <div className="expenses">
             <ExpensesFilter selectedYear={filteredYear} onYearSelected={filterChangeHandler} />
 
-            {props.expenses.map(expense =>
+            {filteredExpenses.map(expense =>
                 <ExpenseItem key={expense.id}
                     title={expense.title}
                     amount={expense.amount}
